def3d3fe81c322f4ae82826692b1d8a40602c6c1
gabriel contreras <gabrielcontreras1426@gmail.com>
2018-11-01 16:39:28 -0500

    menu arreglado

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,40 +1,44 @@
 <template>
 <DefaultLayout>
   <section  class="todo">
-  <nav class="navbar colornav p-0">
+  <nav class="navbar colornav p-0" >
   <a class="navbar-brand">
       <img src='../../assets/logo.png' alt=""> 
   </a>
   <ul class="nav pull-xs-right ">
   <li class="nav-item text-muted p-0">
-
   <button type="button" class="btn btn-outline-secondary">Iniciar Sesion</button>
   </li>
   <li class="nav-item text-muted">
-
   <button type="button" class="btn btn-outline-secondary">Registro</button>
   </li>
   </ul>
   </nav>
-  <div  class="">
+
+  <div  class="container">
+
   <div class="row flex-xl-nowrap">
 
-  <div class="col-12 col-md-3 col-xl-2 bd-sidebar fondocolor  p-0">
+  <div class="col-12 col-md-3 col-xl-2 bd-sidebar fondocolor p-0">
 
-    <b-nav vertical class="w-100 p-0">
-  <ul class="list-group pl-3">
+    <b-navbar vertical class="w-100 p-0" toggleable="md">
+    <b-navbar-toggle   target="nav_collapse"></b-navbar-toggle>
+     <b-collapse class="" is-nav id="nav_collapse">
+       
+      <ul class="list-group w-100  p-0">
   
-  <li class="list-group-item  p-3 lista"><i class="fas fa-home"></i>&nbsp;&nbsp; Predicciones</li>
-  <li class="list-group-item  p-3 lista"><i class="fas fa-futbol"></i> &nbsp;&nbsp;Futball</li>
-  <li class="list-group-item  p-3 lista"><i class="fas fa-chess"></i>&nbsp;&nbsp;Ajedrez</li>
-  <li class="list-group-item  p-3 lista"><i class="fas fa-basketball-ball"></i>&nbsp;&nbsp; Basketball</li>
-  <li class="list-group-item  p-3 lista"><i class="fas fa-paw"></i>&nbsp;&nbsp;Carrera de galgos</li>
-</ul>
-</b-nav>
+        <li class="list-group-item lista p-3"><i class="fas fa-home"></i>&nbsp;&nbsp; Predicciones</li>
+        <li class="list-group-item lista p-3"><i class="fas fa-futbol"></i> &nbsp;&nbsp;football</li>
+        <li class="list-group-item lista p-3"><i class="fas fa-chess"></i>&nbsp;&nbsp;Ajedrez</li>
+        <li class="list-group-item lista p-3"><i class="fas fa-basketball-ball"></i>&nbsp;&nbsp; Basketball</li>
+       <li class="list-group-item lista p-3"><i class="fas fa-paw"></i>&nbsp;&nbsp;Carrera de galgos</li>
+    </ul>
+    </b-collapse>
+   </b-navbar>
 
-    </div>
+  </div>
             
-    <main class="col-12  col-md-9 col-xl-10 p-0  fond1">
+  <main class="col-12  col-md-9 col-xl-10 p-0  fond1">
         
     <template>
   <div  class="mh-100"  style="max-height:200px;">
@@ -52,35 +56,39 @@
     >
 
       <!-- Text slides with image -->
-      <b-carousel-slide caption="First slide"
-                        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+      <b-carousel-slide caption="Bienvenidos Ganadores"
+                        text="Portal web creado con la ultima  tecnologia, para que ud crezca en grande"
                         img-src="https://blog.rockthetraveller.com/wp-content/uploads/2017/11/Los-estadios-de-f%C3%BAtbol-m%C3%A1s-incre%C3%ADbles-del-planeta-slider.jpg"
                         class="mh-100"  style="max-height:200px;"
       ></b-carousel-slide>
 
       <!-- Slides with custom text -->
-      <b-carousel-slide img-src='http://www.smoothiekingcenter.com/assets/img/tulane-slider-aef3ce1a60.jpg'>
-        <h1>Hello world!</h1>
+      <b-carousel-slide img-src='https://assets.metrolatam.com/ec/2017/02/21/futbol-copa-facebook-1200x600.jpg'>
+        <h3>Los Mejores Pronosticos</h3>
+      </b-carousel-slide>
+       <b-carousel-slide img-src='https://www.jazzdeportes.com/img/products/livedealer.jpg'>
+        <h5>Experimenta la tecnologia avanzada y las mejores formulas</h5>
       </b-carousel-slide>
 
-     
     </b-carousel>
 
   </div>
 </template>
 
-        <div class="row flex-xl-nowrap m-2">
+<div class="row flex-xl-nowrap m-2">
 
-        <div class="col-12  col-md-9 col-xl-8 container p-2  ">
-  
-<div>
+  <div class="col-12  col-md-9 col-xl-9 container p-2 ">
+
+      <div class="m-2">
     <b-card no-body
             style="max-width: 100%;"
-      
-            
             img-top>
+        <b-card-header class="p-0 pr-3">
+        <h6  class="m-0"style="text-align:right"> bookmakers  <b-btn v-b-toggle.collapse1 variant="outline-success">Comentarios</b-btn>
+         <b-btn variant="outline-success">seguir</b-btn></h6>
+
+        </b-card-header>
         
-        <h6 slot="header" style="text-align:right"> bookmakers  <b-btn v-b-toggle.collapse1 variant="primary">Comentarios</b-btn> &nbsp;&nbsp; <a href="#">Seguir</a></h6>
         <b-card-body align-self="center" class="p-0" style="text-align: center">
 
             <b-row >
@@ -93,7 +101,6 @@
                   <h3>&nbsp;&nbsp; 3 &nbsp;- &nbsp; 2&nbsp;&nbsp; </h3>
               </b-col>
             </b-row>
-      <hr class="m-1">
           <div class="p-0" style="text-align: left">
         
           <b-collapse id="collapse1" class="mt-2">
@@ -114,25 +121,125 @@
         </div>
                 </b-card-body>
                
-                <b-card-footer style="text-align:right">1 de noviembre del 2018</b-card-footer>
+                <b-card-footer style="text-align:right"><i class="far fa-comments" style='font-size:28px;color:white'> </i> &nbsp;&nbsp;1 de noviembre del 2018</b-card-footer>
                 
             </b-card>
         </div>
 
+
+    <div class="card text-center m-2">
+        <div class="card-header text-right header">
+          bookmakers
         </div>
-        <div class="col-12  col-md-9 col-xl-4 ">
+      <div class="card-body cuerpomensaje ">
+       <h5 class="card-title">Tips luke</h5>
+       <p class="card-text">España - Copa de España CF Villanovense - Sevilla.</p>
+       <a href="#" class="btn btn-primary">Comentar.</a>
+      </div>
+          <div class="card-footer  text-left header ">
+            
+             <div class="card-footer  header ">
+              <a href=""><i class="far fa-comments" style='font-size:28px;color:white'> </i></a> &nbsp;&nbsp;1 days ago 
+          </div> 
+          </div>
+          
+   </div>
+
+  <div class="card text-center m-2">
+        <div class="card-header text-right header">
+          bookmakers
+        </div>
+      <div class="card-body cuerpomensaje ">
+       <h5 class="card-title">Tips AlbeiroJunior</h5>
+       <p class="card-text">España - Copa de España CF Villanovense - Sevilla.</p>
+       <a href="#" class="btn btn-primary">Comentar.</a>
+      </div>
+          <div class="card-footer  text-left header ">
+            
+             <div class="card-footer  header ">
+              <a href=""><i class="far fa-comments" style='font-size:28px;color:white'> </i></a> &nbsp;&nbsp;2 days ago 
+          </div> 
+          </div>
+          
+   </div>
+
 
         </div>
+
+    <div class="col-12 col-md-3 col-xl-3 bd-sidebar fondocolor fondocolorDerecha  p-0">
+
+    <b-nav vertical class="w-100 p-0">
+      <ul class="list-group  pl-3">
+  
+        <li class="list-group-item seccionDerecha  p-3"><i class="fas fa-home"></i>&nbsp;&nbsp; Top Bookmakers</li>
+        <li class="list-group-item seccionDerecha1  p-3"> Coolbet&nbsp;&nbsp;&nbsp;&nbsp;  <i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i> </li>
+         <li class="list-group-item seccionDerecha1  p-3"> Bet365t&nbsp;&nbsp;&nbsp;&nbsp;  <i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i> </li>
+          <li class="list-group-item seccionDerecha1  p-3"> PinnAclet&nbsp;&nbsp;&nbsp;&nbsp;  <i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i> </li>
+           <li class="list-group-item seccionDerecha1  p-3"> Sbobet&nbsp;&nbsp;&nbsp;&nbsp;  <i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i> </li>
+            <li class="list-group-item seccionDerecha1  p-3"> Coolbet&nbsp;&nbsp;&nbsp;&nbsp;  <i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i> </li>
+             <li class="list-group-item seccionDerecha1  p-3"> Betfair&nbsp;&nbsp;&nbsp;&nbsp;  <i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i> </li>
+        
+    </ul>
+   </b-nav>
+
+  </div>
+
         </div>
     </main>
 
-        </div>
+        
     </div>
     
-   
+ </div> <!-- fin dic container  -->
 </section>
 
+
+
+
+
+
+<!-- ffooter  -->
+  <div class="container-fluid" style="height: 200px; background: #000;" >
+    <div class="container">
+
+
+       <div class="row">
+  <div class="col-md-4">
+<h5 class="card-title"  style="color: #ffffff;" >Contactanos</h5>
+                <p class="card-text"  style="color: #ffffff;">With supporting text below as a natural lead-in to additional content.</p>
+</div>
+  <div class="col-md-4">
+   <h5 class="card-title"  style="color: #ffffff;">Nuestros apoyos</h5>
+                <p class="card-text" style="color: #ffffff;">With supporting text below as a natural lead-in to additional content.</p> 
+
+  </div>
+  <div class="col-md-4">
+   <h5 class="card-title"  style="color: #ffffff;">Nuestros apoyos</h5>
+                <p class="card-text" style="color: #ffffff;">With supporting text below as a natural lead-in to additional content.</p> 
+
+  </div>
+
+
+  
+</div>
+    </div>
+
+    
+
+  </div>
+
+    <footer class="py-4 bg-dark">
+
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- fin de footer  -->
+
 	</DefaultLayout>
+
 
 </template>
 <script>
@@ -169,15 +276,15 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#3d3d43', end
 .fondocolor{
   background-color:#2a2a2e;
 }
+.fondocolorDerecha{
+  background-color:transparent;
+
+}
 .btn{
     margin: 10px;
 }
 .btn.fin{
     margin-right:60px;
-}
-.vl {
-    border-left: 1px solid #2a2a2e;
-    height: 100%;
 }
 .btn-outline-secondary{
   color: white;
@@ -185,11 +292,11 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#3d3d43', end
 li:hover{
   background-color: slategray;
 }
-
 .lista{
   background-color: #2a2a2e;
   color:white;
 }
+
 
 .dimensiones{
 
@@ -207,12 +314,30 @@ background: linear-gradient(to bottom, rgba(226,226,226,1) 0%, rgba(219,219,219,
 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#e2e2e2', endColorstr='#fefefe', GradientType=0 );
 }
 
-
-.bg-light{
-  background: #2a2a2e;
-  background-color: transparent!important;
+.seccionDerecha{
+  background-color: #ff7f50;
+  margin-top: 20px;
+  color:white;
+}
+.seccionDerecha1{
+  background-color: #bfece9;
+  display: inline-block;
+  color:blue;
+  font-weight: bold;
 }
 
+.card-header{
+  background-color:#133337;
+  color:white;
+}
+.card-footer{
+  background-color:#133337;
+  color:white;
+}
+.card-body{
+  background-color: #f5f5dc;
+  
+}
 
 </style>
 
